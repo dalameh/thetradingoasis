9e3dd8d9bf9815d2171d45e757babe1e76628fa1
dalameh <dalameh@terpmail.umd.edu>
2025-09-08 15:57:44 -0400

    fixes a

--- app/api/toptickers/route.ts
+++ app/api/toptickers/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
-import { updateTickers } from '../../../lib/scrapeTopTickers';
+// import { updateTickers } from '../../../lib/scrapeTopTickers';
 
 export async function GET() {
   try {
-    await updateTickers();
+    // await updateTickers();
     return NextResponse.json({ message: "Tickers updated" });
   } catch (error) {
     return NextResponse.json(
